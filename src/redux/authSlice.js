11ce133ce974ef4c06cfc4e7633a9d474c4c366e
nguyen-gia-khoi <giakhoi2004@gmail.com>
2024-10-17 20:53:29 +0700

    feat/signup

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -8,6 +8,18 @@ const autheSlice = createSlice({
             isFetching: false,
             error: false,
         },
+        register: {
+            isFetching: false,
+            error: false,
+            success: false,
+            otpSent: false, // Track if OTP is sent
+            otpVerified: false, // Track OTP verification status
+        },
+        verify: {
+            isFetching: false,
+            error: false,
+            success: false,
+        },
     },
     reducers: {
         loginStart: (state) => {
@@ -15,15 +27,61 @@ const autheSlice = createSlice({
         },
         loginSuccess: (state, action) => {
             state.login.isFetching = false;
-            state.login.currentUser = action.payload;
+            state.login.currentUser=action.payload
             state.login.error = false;
         },
         loginFailed: (state) => {
             state.login.isFetching = false;
             state.login.error = true;
         },
+        registerStart: (state) => {
+            state.register.isFetching = true;
+        },
+        registerSuccess: (state) => {
+            state.register.isFetching = false;
+            state.register.error = false;
+            state.register.success = true;
+        },
+        registerFailed: (state) => {
+            state.login.isFetching = false;
+            state.login.error = true;
+            state.register.success = false;
+        },
+        otpSent: (state) => { 
+            state.register.otpSent = true; 
+        },
+        otpFailed: (state) => { 
+            state.register.error = true; 
+        },
+        otpVerified: (state) => { 
+            state.register.otpVerified = true; 
+        },
+        verifyAccountStart: (state) => {
+            state.verify.isFetching = true;
+        },
+        verifyAccountSuccess: (state) => {
+            state.verify.isFetching = false;
+            state.verify.success = true;
+            state.verify.error = false;
+        },
+        verifyAccountFailed: (state) => {
+            state.verify.isFetching = false;
+            state.verify.error = true;
+            state.verify.success = false;
+        },
     },
 });
 
-export const { loginStart, loginFailed, loginSuccess } = autheSlice.actions;
+export const { loginStart, 
+               loginFailed, 
+               loginSuccess,
+               registerStart,
+               registerSuccess,
+               registerFailed,
+               otpSent, 
+               otpFailed, 
+               otpVerified,
+               verifyAccountStart,
+               verifyAccountSuccess,
+               verifyAccountFailed } = autheSlice.actions;
 export default autheSlice.reducer;
