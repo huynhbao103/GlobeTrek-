243e5f8a2ba543a5ad7869cb92afd1eddfc24983
Pham Manh Tien <abc160cba@gmail.com>
2024-11-23 19:02:25 +0700

    fix/conflictS1

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ import SavedPassengers from './YourAccount/savedPassengers.jsx';
 import Refunds from './YourAccount/Refunds.jsx';
 import Savelist from './header1/Savelist.jsx';
 import LoadingLogin from './LoadingLogin';
-// import ForgotPassword from './header1/Password/ForgotPassword.jsx';
 import ForgotPassword from './header1/Password/forgotPassword.jsx';
 // import ResetPassword from './header1/Password/ResetPassword.jsx';
 
--- src/header1/Login.jsx
+++ src/header1/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 import SsoPointerSignIn from "./GoogleSignIn";
 import { useDispatch } from "react-redux"; // Redux Hook
 import { registerUser, signinUser, verifyAccount } from "../redux/apiRequest";
