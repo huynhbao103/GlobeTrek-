8fe697b4d980bb55c6553b92461c3bbe50c0e026
Bao <huynhbao103@gmail.com>
2024-11-23 20:07:29 +0700

    deo fix nua

--- src/YourAccount/SecurityPassword.jsx
+++ src/YourAccount/SecurityPassword.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import Switch from './Switch'
 import Tooltip from './Tooltip'
--- src/YourAccount/UserAccount.jsx
+++ src/YourAccount/UserAccount.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ProfileUser from './ProfileUser';
 import AddEmail from './AddEmail';
