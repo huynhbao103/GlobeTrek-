568c58ad8f086bf5cf851078a969bdf3bd58fd49
Bao <huynhbao103@gmail.com>
2024-11-23 19:28:14 +0700

    fix error vervel

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Body from './main/Body.jsx';
